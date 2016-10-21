d5a390317dc156791dbb7b52cfa51f85e410e2ae
alincode <alincode@gmail.com>
2016-10-22 01:06:51 +0800

    demo 2

---
+++ test/specs/demo2.js
@@ -0,0 +1,33 @@
+var assert = require('assert');
+
+describe('demo 2', function() {
+
+  beforeEach(function() {
+    browser.pause(8000);
+  });
+
+  it('login failure', function() {
+    browser.url('http://demo.keystonejs.com/keystone/signin');
+    browser.element('[name=email]').setValue('demo@keystonejs.com');
+    browser.element('[name=password]').setValue('1111');
+    browser.click('button[type=submit]');
+    browser.waitForExist('.Alert--danger');
+    const message = browser.getText('.Alert--danger');
+    assert.equal(message,
+      'The email and password you entered are not valid.');
+  });
+
+  it('login successful', function() {
+    browser.url('http://demo.keystonejs.com/keystone/signin');
+    browser.element('[name=email]').setValue('demo@keystonejs.com');
+    browser.element('[name=password]').setValue('demo');
+    browser.click('button[type=submit]');
+    browser.waitForExist('[href="/keystone/signout"]');
+  });
+
+  it('logout', function() {
+    browser.click('[href="/keystone/signout"]');
+    browser.waitForExist('.Alert--info');
+  });
+
+});
